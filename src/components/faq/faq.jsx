d591a64ee79f13e02f44d0c93dc7684e20f219ce
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-05-14 14:25:56 +0500

    faq bitdi

--- src/components/faq/faq.jsx
+++ src/components/faq/faq.jsx
@@ -1,47 +1,42 @@
 import React from 'react'
 import "./faq.css"
+import { useTranslation } from 'react-i18next'
 
 export const Faq = () => {
 
-
+ const {t, i18n}= useTranslation
 
   return (
     <>
       <section class="faq">
         <div class="container">
           <div class="faq__left">
-            <h2 class="faq__logo">FAQ</h2>
+            <h2 class="faq__logo">{t("faq.title")}</h2>
           </div>
 
           <div class="faq__right">
             <div class="faq__item">
               <div class="faq__title">
-                <h3>Kursdan nima topaman</h3>
+                <h3>{t("faq.list_title")}</h3>
                 <button class="faq__btn">⌄</button>
               </div>
-              <p class="faq__text">
-                Bu bir nonti kaypidaiy kapy darsga qatnasha olmasangiz, darsdan
-                so‘ng guruhingizga zoom zapisi tashlab beriladi
-              </p>
+              <p class="faq__text">{t("faq.list_text")}</p>
             </div>
 
             <div class="faq__item">
               <div class="faq__title">
-                <h3>Kursdan nima topaman</h3>
+                <h3>{t("faq.list_title")}</h3>
                 <button class="faq__btn">⌄</button>
               </div>
-              <p class="faq__text">Bu yerda boshqa text bo‘ladi</p>
+              <p class="faq__text">{t("faq.list_text")}</p>
             </div>
 
             <div class="faq__item">
               <div class="faq__title">
-                <h3>Kursdan nima topaman</h3>
+                <h3>{t("faq.list_title")}</h3>
                 <button class="faq__btn">⌄</button>
               </div>
-              <p class="faq__text">
-                Bu bir nonti kaypidaiy kapy darsga qatnasha olmasangiz, darsdan
-                so‘ng guruhingizga zoom zapisi tashlab beriladi
-              </p>
+              <p class="faq__text">{t("faq.list_text")}</p>
             </div>
           </div>
         </div>
