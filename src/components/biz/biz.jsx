c594eeca407b2612fe3cc6f7c2b2c3f014d7d30b
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-05-14 14:20:08 +0500

    biz sectioni bitdi

--- src/components/biz/biz.jsx
+++ src/components/biz/biz.jsx
@@ -3,20 +3,18 @@ import "./biz.css"
 
 
 import xalat from "./oq xalat.png"
+import { useTranslation } from 'react-i18next'
 
 export const Biz = () => {
+  const {t, i18n} = useTranslation()
   return (
     <>
      <section class="biz">
         <div class="container">
           <img src={xalat} alt="" class="biz_img" />
       /    <div class="biz_content">
-            <h2 class="biz_title">
-              Bizga ko'p yillardan beri <br />
-              kompaniyamizga ishonch <br />
-              bildirganlar talaygina
-            </h2>
-            <button class="biz_btn">Bog’lanish</button>
+            <h2 class="biz_title">{t("biz.title")} </h2>
+            <button class="biz_btn">{t("biz.button")}</button>
           </div>
         </div>
       </section>
